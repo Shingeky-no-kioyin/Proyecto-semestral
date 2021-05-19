bfc83041442a56164df483c48f1b075e9c7d27d7
Shingeky-no-kioyin <er.ruarte@duocuc.cl>
2021-05-19 16:25:48 -0400

    VALIDAR

--- js/script.js
+++ js/script.js
@@ -1,3 +1,89 @@
+//VALIDANDO CONTÁCTANOS.
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{8,14}$/ // 8 a 14 números.
+}
+
+const campos = {
+	nombre: false,
+	correo: false,
+	telefono: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) { //comenzará a valizar, quiero comprobar
+		case "nombre": 
+			if(expresiones.nombre.test(e.target.value)){
+                document.getElementById("grupo__nombre").classList.remove("formulario__grupo-incorrecto");
+                document.getElementById("grupo__nombre").classList.add("formulario__grupo-correcto");
+                document.querySelector("#grupo__nombre i").classList.remove("fa-check-circle");
+                document.querySelector("#grupo__nombre i").classList.add("fa-times-circle");
+            } else{
+                document.getElementById('grupo__nombre').classList.add("formulario__grupo-incorrecto");
+                document.getElementById('grupo__nombre').classList.remove("formulario__grupo-correcto");
+                document.querySelector("#grupo__nombre i").classList.add("fa-times-circle");
+                document.querySelector("#grupo__nombre i").classList.remove("fa-check-circle");
+                document.querySelector("#grupo__nombre .formulario__input-error").classList.add("formulario__input-error-activo")
+            }
+		break;
+        case "correo": 
+			validarCampo(expresiones.telefono, e.target, 'telefono'); 
+		break; 
+        case "telefono": 
+			validarCampo(expresiones.telefono, e.target, 'telefono'); 
+		break; 
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
+
+//TERMINÓ LA VALIDACIÓN DE CONTÁCTANOS
+
+
 //Fecha actual
 function fechaActual() {
     var dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
