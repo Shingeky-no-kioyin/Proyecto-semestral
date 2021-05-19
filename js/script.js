f1d45d5d6bd9651a32060a7c3cf93efec6e45854
Shingeky-no-kioyin <er.ruarte@duocuc.cl>
2021-05-19 12:31:54 -0400

    avance 19/05-> 12:31

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $("#txt_contactanos").hide()
-
-    $("#txt_con").blur(function(){
-        var txt1 = $("#txt_contactanos").val()
-        if(txt1.length > 0){
-            console.log("txt1 perdió el foco") 
-            $("#txt_contactanos").html("nombre debe tener como minimo 3 caracteres")
-            $("#txt_contactanos").fadeIn() 
-        }
-    });
-
-    $("#txt_con").focus(function(){
-        console.log("txt_con ganó el foco")
-        $("#txt_contactanos").fadeOut()
-    });
-    $("#form1").submit(function(){
-        console.log("submit")
-
-        var pass = $("#txt2").val()
-
-        if(pass.length < 8){
-            $("#error2").html("contraseña debe tener minimo 8 caracteres")
-            event.preventDefault()
-        }
-    })
-});
-=======
 //Fecha actual
 function fechaActual() {
     var dias = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
@@ -96,4 +67,3 @@ $(document).ready(
         getCoordenadas()
     }
 );
->>>>>>> f956fee21af9f9393fcd12e733bfcdfd7e0c640a
